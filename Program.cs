e5de45ce7a88ff7fda868791f4da373b0cab4102
code at mm <code@mukto-mona.com>
2020-11-15 18:04:02 -0800

    statiq blog initial files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace mmblog {
+    public class Program {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .DeployToGitHubPagesBranch("think-mm", "bnsite", Config.FromSetting<string>("GITHUB_TOKEN"), "web")
+                .RunAsync();
+    }
+}
